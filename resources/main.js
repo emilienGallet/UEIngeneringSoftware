2e9cd6cea0e8ece35cb9f4c57f06629ff1d23001
emilienGallet <emilienflorentclaude.gallet@gmail.com>
2020-10-11 16:28:28 +0200

    [IN-DEV] Put in place format of file, add environement section

--- resources/main.js
+++
@@ -1,9 +0,0 @@
-let section = document.getElementById("vide");
-let footer = document.getElementsByTagName("footer")[0];
-let calculus = document.body.clientHeight - document.body.offsetHeight +8;
-//section.style.height = calculus.toString();
-section.style.height = calculus.toString();
-//console.log("OK");
-
-let flag = false;
-
